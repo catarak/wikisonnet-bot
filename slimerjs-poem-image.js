ff3f7d8255f6f9a08969ecb2c77d4f7e0d4367b6
catarak <ctarakajian@gmail.com>
2016-03-03 13:18:46 -0500

    adjust bounds of poem image

--- slimerjs-poem-image.js
+++ slimerjs-poem-image.js
@@ -15,10 +15,10 @@ page.viewportSize = {
 };
 
 page.clipRect = {
-	top: 45,
-	left: 125,
-	width: 630,
-	height: 750
+	top: 90,
+	left: 190,
+	width: 430,
+	height: 520
 };
 
 page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
